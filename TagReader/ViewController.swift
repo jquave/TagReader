3d2750d7407dd2196374417fa6bb61f40d55ebf5
Jameson Quave <jquave@gmail.com>
2017-06-16 20:43:51 -0500

    Initial Commit

---
+++ TagReader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TagReader
+//
+//  Created by Jameson Quave on 6/16/17.
+//  Copyright © 2017 Jameson Quave. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
